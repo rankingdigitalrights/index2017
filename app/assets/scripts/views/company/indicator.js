75f21370b9c80830d38ddecb5bf6f06a0c743b05
Stevan Postic <stevan.postic@cloudtech.company>
2017-03-07 11:22:41 +0100

    Company page

--- app/assets/scripts/views/company/indicator.js
+++ app/assets/scripts/views/company/indicator.js
@@ -18,90 +18,92 @@ module.exports = BaseChart.extend({
   // the overview, and each category graph.
   render: function (el) {
 
-    var xxx = this.collection;
+    var tmp = this.collection;
     var companyName = this.companyName;
-    var comp = xxx.findWhere({ id: companyName });
-    console.info(comp);
+    var comp = tmp.findWhere({ id: companyName });
 
-    //this.collection.forEach(function (d, i) {
-        var category = ['freedom', 'privacy', 'commitment'];
-        category.forEach(function (xx, yy) {
-            //console.info(x);
-            var data = comp.attributes[xx];
+    var category = ['freedom', 'privacy', 'commitment'];
+    category.forEach(function (i, dd) {
+        
+        //console.info(x);
+        var data = comp.attributes[i];
 
-            //set up svg using margin conventions - we'll need plenty of room on the left for labels
-            var margin = {
-                top: 15,
-                right: 25,
-                bottom: 15,
-                left: 60
-            };
+        //set up svg using margin conventions - we'll need plenty of room on the left for labels
+        var margin = {
+            top: 10,
+            right: 350,
+            bottom: 10,
+            left: 10
+        };
 
-            var width = 200,//460 - margin.left - margin.right,
-                height = 200 - margin.top - margin.bottom;
+        var width = 460 - margin.left - margin.right,
+            height = 25 * data.length; // 200 - margin.top - margin.bottom;
 
-            var svg = d3.select("#indicators--"+xx).append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var x = d3.scale.linear()
-                .range([0, width])
-                .domain([0, d3.max(data, function (d) {
-                    return d.value;
-                })]);
+        var svg = d3.select("#indicators--"+i).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var y = d3.scale.ordinal()
-                .rangeRoundBands([height, 0], .1)
-                .domain(data.map(function (d) {
-                    return d.name;
-                }));
 
-            //make y axis to show bar names
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                //no tick marks
-                .tickSize(0)
-                .orient("left");
+        var x = d3.scale.linear()
+            .domain(d3.extent(data, function(d) { 
+                var neg = d.value * (-1);
+                return neg; 
+            }))
+            .range([0, width]);
 
-            var gy = svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
 
-            var bars = svg.selectAll(".bar")
-                .data(data)
-                .enter()
-                .append("g")
+        //make y axis to show bar names
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .tickSize(0)
+            .orient("right");
 
-            //append rects
-            bars.append("rect")
-                .attr("class", "bar")
-                .attr("y", function (d) {
-                    return y(d.name);
-                })
-                .attr("height", y.rangeBand())
-                .attr("x", 0)
-                .attr("width", function (d) {
-                    return x(d.value);
-                });
+        var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+                .selectAll("text")  
+                .attr("x", 110);
 
-            //add a value label to the right of each bar
-            bars.append("text")
-                .attr("class", "label")
-                //y position of the label is halfway down the bar
-                .attr("y", function (d) {
-                    return y(d.name) + y.rangeBand() / 2 + 4;
-                })
-                //x position is 3 pixels to the right of the bar
-                .attr("x", function (d) {
-                    return x(d.value) + 3;
-                })
-                .text(function (d) {
-                    return d.value;
-            });
+        var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) {
+                var neg = d.value * (-1); 
+                return x(Math.min(0, neg)); 
+            })
+            .attr("y", function(d) { return y(d.name); })
+            .attr("width", function(d) {
+                var neg = d.value * (-1);
+                return Math.abs(x(neg) - x(0)); 
+            })
+            .attr("height", y.rangeBand());
+            
 
-        })
-    //});
+        //add a value label to the right of each bar
+        /*
+        bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.value) + 3;
+            })
+            .text(function (d) {
+                return d.value;
+        });
+        */
+    })
   },
 });
