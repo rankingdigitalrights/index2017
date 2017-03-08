66828ba0b60c4984bfc9db1141020347c333b6f7
Stevan Postic <stevan.postic@cloudtech.company>
2017-03-08 16:01:57 +0100

    Indicators

--- app/assets/scripts/collections/company/indicator.js
+++ app/assets/scripts/collections/company/indicator.js
@@ -5,6 +5,7 @@ var indicator = require('../../models/company/indicator');
 
 module.exports = Backbone.Collection.extend({
   model: indicator,
+  
   url: baseurl + '/assets/static/company/all-indicators.json',
   parse: function (resp) {
     return resp;
